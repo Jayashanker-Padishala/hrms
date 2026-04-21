55ee832c0a50aba1327fb1f62e117f798e6309dd
iamkhanraheel <raheel@frappe.io>
2026-04-21 17:43:42 +0530

    fix(leave_application): validate leave access for approver & employee

--- hrms/hr/doctype/leave_application/leave_application.py
+++ hrms/hr/doctype/leave_application/leave_application.py
@@ -990,7 +990,7 @@ def get_number_of_leave_days(
 
 @frappe.whitelist()
 def get_leave_details(employee: str, date: str | datetime.date, for_salary_slip: bool = False) -> dict:
-	frappe.has_permission("Employee", "read", employee, throw=True)
+	validate_leave_access(employee)
 
 	allocation_records = get_leave_allocation_records(employee, date)
 	leave_allocation = {}
@@ -1051,8 +1051,7 @@ def get_leave_balance_on(
 	        if True, returns a dict eg: {'leave_balance': 10, 'leave_balance_for_consumption': 1}
 	        else, returns leave_balance (in this case 10)
 	"""
-	if frappe.request:
-		frappe.has_permission("Employee", "read", employee, throw=True)
+	validate_leave_access(employee)
 
 	if not to_date:
 		to_date = nowdate()
@@ -1556,3 +1555,11 @@ def get_leave_approver_and_mandatory(employee: str) -> dict:
 		"is_mandatory": 1 if mandatory else 0,
 		"leave_approver": get_leave_approver(employee),
 	}
+
+
+def validate_leave_access(employee):
+	employee_user = frappe.db.get_value("Employee", employee, "user_id")
+	leave_approver = get_leave_approver(employee)
+
+	if frappe.session.user not in (employee_user, leave_approver, "Administrator"):
+		frappe.throw(_("Not permitted"), frappe.PermissionError)
